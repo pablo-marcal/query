42f9ff70bcc1cfba8f92fa60e3781af4f2db569e
unknown <helievysonp@gmail.com>
2024-05-23 14:35:21 -0300

    queryum

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// const p = document.getElementsByTagName("p")
+//  const p = document.getElementsByClassName("texto")
+// const p = document.querySelector("#texto")
+// const p = document.querySelectorAll("p")
+// console.log(p)
+
